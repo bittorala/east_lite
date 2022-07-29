bbf4fe4cef781441d633d96c8c276039d10b2385
bittorala <balana@ucm.es>
2022-07-29 13:19:07 +0200

    Fix CORS and preview image and results

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from typing import Union
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 import uuid
 from io import BytesIO
 import numpy as np
@@ -12,6 +13,10 @@ m = model()
 m.load_weights('/tmp/ckpt/ckpt')
 
 app = FastAPI()
+
+origins = ["http://localhost", "http://localhost:3000"]
+
+app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
 @app.get("/")
 def read_root():
